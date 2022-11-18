5544b4ecaf500ecaa751ee6149d35cc4d21812c4
smrazvan <razvanro666@gmail.com>
2022-11-18 16:53:52 +0200

    Add project files.

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+
+    }
+}
