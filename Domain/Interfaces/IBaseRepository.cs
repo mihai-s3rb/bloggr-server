a6f2c607b1505a3e851170f4370c3bb5fadd380e
smrazvan <razvanro666@gmail.com>
2022-11-28 16:40:44 +0200

    added basic interface for generic base repo

---
+++ Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloggr.Domain.Interfaces
+{
+    internal interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        void AddPost(TEntity entity);
+    }
+}
